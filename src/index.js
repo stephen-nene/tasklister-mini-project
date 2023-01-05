320b7e8d63ddfed25e951a2ab158ac9fa87eb0b3
+stephen.nene <+>
2023-01-05 05:55:38 +0300

    done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+    //inputs
+    const createdTaskForm = document.getElementById("create-task-form");
+    const newTaskDescription = document.getElementById("new-task-description");
+    const newTaskPriority = document.getElementById("new-task-priority");
+    
+    // where the new tasks at
+    
+    const newTasks = document.getElementById("tasks");
+    
+    createdTaskForm.addEventListener("submit", spawnTaskForm, );
+    });
+    
+     spawnTaskForm = event => {
+      event.preventDefault();
+      const newTaskDescription = document.getElementById("new-task-description");
+      const anotherTask = document.createElement("li")
+      anotherTask.innerHTML = newTaskDescription.value;
+    
+    
+    appendAnotherTask(anotherTask);
+      event.target.reset();
+    };
+    
+    const appendNewestTask = task => {
+      document.getElementById('tasks').appendChild(task);
+    };
+
